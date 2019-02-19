1c787a245cccc78ca8f0a4fcb0ded85c13e130a8
Ramesh AMARCHAND SHARMA <ramesh.amarchand.sharma@ing.com>
2019-02-19 11:54:45 +0100

    Spring-Cloud-Contract: Junit test case addition.

--- src/test/java/com/scc/springcloudcontract/controller/ControllerClassTest.java
+++ src/test/java/com/scc/springcloudcontract/controller/ControllerClassTest.java
@@ -1,5 +1,6 @@
 package com.scc.springcloudcontract.controller;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,18 +17,12 @@ import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilde
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-//@AutoConfigureStubRunner(
-//        stubsMode = StubRunnerProperties.StubsMode.REMOTE,
-//        repositoryRoot = "git://https://github.com/jabrena/spring-cloud-contract-git.git",
-//        ids = {
-//                "org.jab.microservices:spring-cloud-contract-git-producer-poc:0.1.0-SNAPSHOT",
-//                "org.jab.microservices:spring-cloud-contract-git-producer-poc2:0.1.0-SNAPSHOT"
-//        })
 @AutoConfigureStubRunner(stubsMode = StubRunnerProperties.StubsMode.REMOTE,
         repositoryRoot = "git://https://github.com/xjuste/sccpoc.git",
         ids = {"com.toutjuste:sccpoc:0.0.1-SNAPSHOT:stubs:8092"
@@ -40,13 +35,72 @@ public class ControllerClassTest {
     MockMvc mvc;
 
     @Test
-    public void getDetails() throws Exception {
+    public void given_Stubs_When_CallingStubsLocally_Then_ReturnSuccessReponse() throws Exception {
 
         MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/endpoint1");
 
         this.mvc.perform(builder)
                 .andDo(print())
                 .andExpect(status()
-                        .isOk());
+                        .isOk())
+        .andExpect(content().json("[\n" +
+                "\t{\n" +
+                "\t\t'userId': 1,\n" +
+                "\t\t'id': 1,\n" +
+                "\t\t'title': 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',\n" +
+                "\t\t'body': 'quia et suscipit\\nsuscipit recusandae consequuntur expedita et cum\\nreprehenderit molestiae ut ut quas totam\\nnostrum rerum est autem sunt rem eveniet architecto'\n" +
+                "\t},\n" +
+                "\t{\n" +
+                "\t\t'userId': 1,\n" +
+                "\t\t'id': 2,\n" +
+                "\t\t'title': 'qui est esse',\n" +
+                "\t\t'body': 'est rerum tempore vitae\\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\\nqui aperiam non debitis possimus qui neque nisi nulla'\n" +
+                "\t},\n" +
+                "\t{\n" +
+                "\t\t'userId': 1,\n" +
+                "\t\t'id': 3,\n" +
+                "\t\t'title': 'ea molestias quasi exercitationem repellat qui ipsa sit aut',\n" +
+                "\t\t'body': 'et iusto sed quo iure\\nvoluptatem occaecati omnis eligendi aut ad\\nvoluptatem doloribus vel accusantium quis pariatur\\nmolestiae porro eius odio et labore et velit aut'\n" +
+                "\t}\n" +
+                "]"));
+    }
+
+
+    @Test(expected = AssertionError.class)
+    public void given_Stubs_When_AssertingIncorrectJsonStaticResponse_Then_ThrowAssertionError() throws Exception {
+
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/endpoint1");
+
+        this.mvc.perform(builder)
+                .andDo(print())
+                .andExpect(status()
+                        .isOk())
+                .andExpect(content().json("[\n" +
+                        "\t{\n" +
+                        "\t\t'userId': 1,\n" +
+                        "\t\t'id': 1,\n" +
+                        "\t\t'title': 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',\n" +
+                        "\t\t'body': 'quia et suscipit\\nsuscipit recusandae consequuntur expedita et cum\\nreprehenderit molestiae ut ut quas totam\\nnostrum rerum est autem sunt rem eveniet architecto'\n" +
+                        "\t},\n" +
+                        "\t{\n" +
+                        "\t\t'userId': 1,\n" +
+                        "\t\t'id': 2,\n" +
+                        "\t\t'title': 'qui est esse',\n" +
+                        "\t\t'body': 'est rerum tempore vitae\\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\\nqui aperiam non debitis possimus qui neque nisi nulla'\n" +
+                        "\t},\n" +
+                        "\t{\n" +
+                        "\t\t'userId': 1,\n" +
+                        "\t\t'id': 3,\n" +
+                        "\t\t'title': 'ea molestias quasi exercitationem repellat qui ipsa sit aut',\n" +
+                        "\t\t'body': 'et iusto sed quo iure\\nvoluptatem occaecati omnis eligendi aut ad\\nvoluptatem doloribus vel accusantium quis pariatur\\nmolestiae porro eius odio et labore et velit aut'\n" +
+                        "\t},\n" +
+                        "\t{\n" +
+                        "\t\t'userId': 1,\n" +
+                        "\t\t'id': 4,\n" +
+                        "\t\t'title': 'ea molestias quasi exercitationem repellat qui ipsa sit aut',\n" +
+                        "\t\t'body': 'et iusto sed quo iure\\nvoluptatem occaecati omnis eligendi aut ad\\nvoluptatem doloribus vel accusantium quis pariatur\\nmolestiae porro eius odio et labore et velit aut'\n" +
+                        "\t}\n" +
+                        "]"));
+
     }
 }
